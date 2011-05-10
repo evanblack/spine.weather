59443240829a9b52a07131a870f422e75334bc54
evanblack <evanmblack@gmail.com>
2011-05-10 17:48:02 -0500

    Added YQL Plugin to handle web service queries

---
+++ js/plugins/yql.js
@@ -0,0 +1,21 @@
+(function($){
+  
+  var defaults = {
+    diagnostics:    "false",
+    cbfunc:			"?",
+    format:     	"json"
+  };
+  
+  $.yql = function( query, callback, params ) {
+  	if ( !query || !callback ) throw("query and callback required");
+  	
+    var params = $.extend( defaults, params );
+    params.queryString = query;  
+    
+    return $.getJSON("http://query.yahooapis.com/v1/public/yql?q="+params.queryString+"&format="+params.format+"&diagnostics="+params.diagnostics+"&callback="+params.cbfunc, function(data, status){
+      callback(data.query.results);
+    });    
+  	
+  };
+
+})(this.Zepto);
